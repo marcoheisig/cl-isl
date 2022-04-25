8ee5df2297d28ca2686b376a126ba70e3c80df70
ltc <abc1230zz@mail.ru>
2022-04-25 15:18:18 +0200

    First test of reorder works

--- demo.lisp
+++ demo.lisp
@@ -13,8 +13,101 @@
   )
 
 
-(defmacro break2 (a) ())
+(defvar s-domain)
+(setf s-domain "[n] -> {
+      S[k] : k <= -2 + 2n and k >= 0;
+      T[i, j] : i >= 0 and i <= -1 + n and j <= -1 + n and j >= 0;
+}")
 
+(defvar s-read)
+(setf s-read "[n] -> {
+    T[i, j] -> C[i + j];
+    T[i, j] -> B[j];
+    T[i, j] -> A[i];
+}")
+
+(defvar s-write)
+(setf s-write "[n] -> {
+    S[k] -> C[k];
+    T[i, j] -> C[i + j];
+}")
+
+(defvar s-schedule)
+(setf s-schedule "[n] -> {
+      T[i, j] -> [1, i, j];
+      S[k] -> [0, k, 0];
+}")
+
+;; Generics functions
+(defun-with-type inverse ((e isl-union_map take))
+  (create-union_map (isl_union_map_reverse (obj e))))
+
+(defun-with-type intersect ((e isl-union_map take) (d isl-union_set take))
+  (create-union_map (isl_union_map_intersect_domain (obj e) (obj d))))
+
+(defun-with-type intersect-map ((e isl-union_map take) (m isl-union_map take))
+  (create-union_map (isl_union_map_intersect (obj e) (obj m))))
+
+(defun-with-type union-map ((a isl-union_map take) (b isl-union_map take))
+  (create-union_map (isl_union_map_union (obj a) (obj b))))
+
+(defun-with-type jsp ((a isl-union_map take) (b isl-union_map take))
+  (create-union_map (isl_union_map_lex_lt_union_map (obj a) (obj b))))
+
+(defun-with-type before ((a isl-union_map take) (b isl-union_map take))
+  (create-union_map (isl_union_map_apply_range (obj a) (obj b))))
+
+;; The error printed when we call intersect with the wrongs types is really bad
+
+
+(let* ((domain (union_set-read-from-str s-domain))
+       (read-access (union_map-read-from-str s-read))
+       (write-access (union_map-read-from-str s-write))
+       (initial-schedule (union_map-read-from-str s-schedule))
+       (before (jsp initial-schedule initial-schedule))
+
+       (read-access (intersect read-access domain))
+       (write-access (intersect write-access domain))
+
+       (RaW (intersect-map (before write-access (inverse read-access)) before))
+       (WaW (intersect-map (before write-access (inverse write-access)) before))
+       (WaR (intersect-map (before read-access (inverse write-access)) before))
+
+       (total (union-map (union-map RaW WaW) WaR))
+
+
+       (ast-build (alloc-ast_build))
+       (schedule (create-schedule-on-domain domain))
+       (_ (break2 "Domain done"))
+       (schedule (schedule-constraints-set-validity schedule total))
+       (_ (break2 "Validity done"))
+       (_ (break2 "End of creation of the schedule"))
+       (schedule (schedule-constraints-compute-schedule schedule))
+       (_ (break2 "Schedule created"))
+       (node (ast_build-node-from-schedule ast-build schedule))
+       (_ (break2 "Ast created"))
+       )
+
+  (print (ast_node-to-C-str node))
+
+;;(print read-access)
+;;(print (inverse read-access))
+;;(print RaW)
+
+)
+
+
+
+
+
+
+
+
+
+;; END
+
+(defmacro break2 (a) ())
+(when nil
 (let ((t1 "{TAB[I] -> [I,1] : 0 <= I < 100;T2[I] -> [I,2] : 0 <= I < 100;}")
       (t2 "{TAB[I,J] -> [I,2,J,3] : 0 <= I < 100 and 0 <= J < 100;}")
       (t3 "{TAB[I,J] -> [I,3,J,4] : 0 <= I < 100 and 0 <= J < 100;}")
@@ -66,22 +159,4 @@
          )
     (print (ast_node-to-C-str node)))
 
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  ))
