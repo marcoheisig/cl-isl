5b0f96d4ebd5ddeb0215b7605938a200142f4141
ltc <abc1230zz@mail.ru>
2022-06-13 10:58:23 +0200

    Readme and demo

--- demo.lisp
+++ demo.lisp
@@ -1,213 +1,113 @@
 (ql:quickload :cl-isl)
 
-(in-package #:cl-isl)
-
-;; Test
-(let* ((a (basic_set-read-from-str "{[i] : exists (a : i = 2a and i >= 10 and i <= 42)}"))
-       (emp (create-empty-basic_set)))
-  (print (empty-basic_set-p emp))
-  (print (empty-basic_set-p a))
-  (print (obj a))
-  (assert (empty-basic_set-p emp))
-  (assert (not (empty-basic_set-p a)))
-  )
-
-(defmacro break2 (a) ())
-
-(defvar s-domain)
-(defvar s-read)
-(defvar s-write)
-(defvar s-schedule)
-
-(when nil
-(setf s-domain "[n] -> {
-      S[k] : k <= -2 + 2n and k >= 0;
-      T[i, j] : i >= 0 and i <= -1 + n and j <= -1 + n and j >= 0;
-}")
-
-(setf s-read "[n] -> {
-    T[i, j] -> C[i + j];
-    T[i, j] -> B[j];
-    T[i, j] -> A[i];
-}")
-
-(setf s-write "[n] -> {
-    S[k] -> C[k];
-    T[i, j] -> C[i + j];
-}")
-
-(setf s-schedule "[n] -> {
-      T[i, j] -> [1, i, j];
-      S[k] -> [0, k, 0];
-}")
-)
-
-(setf s-domain t4domain)
-(setf s-schedule t4schedule)
-(setf s-read t4read)
-(setf s-write t4write)
 
 
-;; Generics functions
-(defun-with-type inverse ((e isl-union_map take))
-  (create-union_map (isl_union_map_reverse (obj e))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Simple intersection/union of sets
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun-with-type intersect ((e isl-union_map take) (d isl-union_set take))
-  (create-union_map (isl_union_map_intersect_domain (obj e) (obj d))))
+;; { [1] } and { [i] }
 
-(defun-with-type intersect-map ((e isl-union_map take) (m isl-union_map take))
-  (create-union_map (isl_union_map_intersect (obj e) (obj m))))
+;; #<CL-ISL:UNION-SET { [1] }>
+(let ((a (isl:union-set-from-str " { [1] } "))
+      (b (isl:union-set-from-str " { [i] } ")))
+  (isl:union-set-intersect a b))
 
-(defun-with-type union-map ((a isl-union_map take) (b isl-union_map take))
-  (create-union_map (isl_union_map_union (obj a) (obj b))))
+;; #<CL-ISL:UNION-SET { [i] }>
+(let ((a (isl:union-set-from-str " { [1] } "))
+      (b (isl:union-set-from-str " { [i] } ")))
+  (isl:union-set-union a b))
 
-(defun-with-type jsp ((a isl-union_map take) (b isl-union_map take))
-  (create-union_map (isl_union_map_lex_lt_union_map (obj a) (obj b))))
 
-(defun-with-type before ((a isl-union_map take) (b isl-union_map take))
-  (create-union_map (isl_union_map_apply_range (obj a) (obj b))))
+;; { [1] } and { [i]: i >= 10 }
 
-;; The error printed when we call intersect with the wrongs types is really bad
+;; #<CL-ISL:UNION-SET {  }>
+(let ((a (isl:union-set-from-str " { [1] } "))
+      (b (isl:union-set-from-str " { [i]: i >= 10 } ")))
+  (isl:union-set-intersect a b))
 
-(defun get-perfect-schedule (s-domain s-read s-write s-schedule)
-(let* ((domain (union_set-read-from-str s-domain))
-       (read-access (union_map-read-from-str s-read))
-       (write-access (union_map-read-from-str s-write))
-       (initial-schedule (union_map-read-from-str s-schedule))
-       (before (jsp initial-schedule initial-schedule))
+;; <CL-ISL:UNION-SET { [i0] : i0 >= 10; [1] } >
+(let ((a (isl:union-set-from-str " { [1] } "))
+      (b (isl:union-set-from-str " { [i]: i >= 10 } ")))
+  (isl:union-set-union a b))
 
-       (read-access (intersect read-access domain))
-       (write-access (intersect write-access domain))
 
-       (RaW (intersect-map (before write-access (inverse read-access)) before))
-       (WaW (intersect-map (before write-access (inverse write-access)) before))
-       (WaR (intersect-map (before read-access (inverse write-access)) before))
+;; { [i]: i >= -5 } and { [i]: i <= 5 }
 
-       (total (union-map (union-map RaW WaW) WaR))
+;; #<CL-ISL:UNION-SET { [i] : -5 <= i <= 5 }>
+(let ((a (isl:union-set-from-str " { [i]: i >= -5 } "))
+      (b (isl:union-set-from-str " { [i]: i <= 5 } ")))
+  (isl:union-set-intersect a b))
+
+;; #<CL-ISL:UNION-SET { [i] : i >= -5 or i <= 5 }>
+(let ((a (isl:union-set-from-str " { [i]: i >= -5 } "))
+      (b (isl:union-set-from-str " { [i]: i <= 5 } ")))
+  (isl:union-set-union a b))
 
 
 
-       (schedule (create-schedule-on-domain domain))
-       (_ (break2 "Domain done"))
-       (schedule (schedule-constraints-set-validity schedule total))
-       (_ (break2 "Validity done"))
-       ;;(schedule (schedule-constraints-set-coincidence schedule (union_map-read-from-str "{ LINE1[i, j, k] -> LINE1[i, j+1, k] : 0 <= i <= 99 and 0 <= j <= 99 and 0 <= k <= 99 }")))
-       ;;(schedule (schedule-constraints-set-coincidence schedule (union_map-read-from-str "{ B[i, j] -> B[i, j+1] : 0 <= i <= 99 and 0 <= j <= 99 }")))
-       (schedule (schedule-constraints-set-coincidence schedule RaW))
-       (_ (break2 "End of creation of the schedule"))
-       (schedule (schedule-constraints-compute-schedule schedule))
-       )
-  schedule))
+;;;;;;;;;;;;;;;;;;;;;
+;; Simple use of maps
+;;;;;;;;;;;;;;;;;;;;;
 
-(defun perfect-code (s-domain s-read s-write s-schedule)
-  (let* ((schedule (get-perfect-schedule s-domain s-read s-write s-schedule))
-         (ast-build (alloc-ast_build))
-         (node (ast_build-node-from-schedule ast-build schedule)))
-    (ast_node-to-C-str node)))
+;; #<CL-ISL:UNION-MAP { [i] -> [2 + i] }>
+(let ((a (isl:union-map-from-str " { [i] -> [i + 1] } ")))
+  (isl:union-map-apply-range a a))
 
+;; #<CL-ISL:UNION-SET { B[6]; B[5] }>
+(let ((a (isl:union-map-from-str "{ A [2 ,8 ,1] -> B [5]; A [2 ,8 ,1] -> B [6]; B [5] -> B [5] } "))
+      (b (isl:union-set-from-str " { A [2 ,8 ,1]; B [5] } ")))
+  (isl:union-set-apply b a))
 
-(let* ((domain (union_set-read-from-str s-domain))
-       (read-access (union_map-read-from-str s-read))
-       (write-access (union_map-read-from-str s-write))
-       (initial-schedule (union_map-read-from-str s-schedule))
-       (before (jsp initial-schedule initial-schedule))
-
-       (read-access (intersect read-access domain))
-       (write-access (intersect write-access domain))
-
-       (RaW (intersect-map (before write-access (inverse read-access)) before))
-       (WaW (intersect-map (before write-access (inverse write-access)) before))
-       (WaR (intersect-map (before read-access (inverse write-access)) before))
-
-       (total (union-map (union-map RaW WaW) WaR))
+;; #<CL-ISL:UNION-SET { [i0] : i0 <= 10 }>
+(let ((a (isl:union-map-from-str " { [i] -> [i+1] } "))
+      (b (isl:union-set-from-str " { [i]: i <= 9 } ")))
+  (isl:union-set-apply b a))
 
 
 
-       (ast-build (alloc-ast_build))
-       (schedule (create-schedule-on-domain domain))
-       (_ (break2 "Domain done"))
-       (schedule (schedule-constraints-set-validity schedule total))
-       (_ (break2 "Validity done"))
-       ;;(schedule (schedule-constraints-set-coincidence schedule (union_map-read-from-str "{ LINE1[i, j, k] -> LINE1[i, j+1, k] : 0 <= i <= 99 and 0 <= j <= 99 and 0 <= k <= 99 }")))
-       ;;(schedule (schedule-constraints-set-coincidence schedule (union_map-read-from-str "{ B[i, j] -> B[i, j+1] : 0 <= i <= 99 and 0 <= j <= 99 }")))
-       (schedule (schedule-constraints-set-coincidence schedule RaW))
-       (_ (break2 "End of creation of the schedule"))
-       (schedule (schedule-constraints-compute-schedule schedule))
-       (_ (break2 "Schedule created"))
-       (node (ast_build-node-from-schedule ast-build schedule))
-       (_ (break2 "Ast created"))
-       )
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Example of utilisation
+;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(in-package :cl-isl)
 
-  (print (ast_node-to-C-str node))
+;; The domain of iteration of iteration the outer loop is { [i, j] : 1 <= i < 10 and 1 <= j < 10 }
+(setf domain-outer (union-set-from-str " { [i, j] : 1 <= i < 10 and 1 <= j < 10 } "))
+;; #<UNION-SET { [i, j] : 0 < i <= 9 and 0 < j <= 9 }>
 
-;;(print read-access)
-;;(print (inverse read-access))
-;;(print RaW)
+;; The domain of iteration of iteration the inner loop is [i] -> { [i, j] : 1 <= j < 10 }
+(setf domain-inner (union-set-from-str " [i] -> { [i, j] : 1 <= j < 10 } "))
+;; #<UNION-SET [i] -> { [i, j] : 0 < j <= 9 }>
 
+;; What is read in the instruction in the inner loop is { [i, j] -> array[1] }
+(setf vread (union-map-from-str " { [i, j] -> array[1] } "))
+;; #<UNION-MAP { [i, j] -> array[1] }>
 
-  )
+;; What is written in the instruction in the inner loop is { [i, j] -> array[i+j] }
+(setf vwrite (union-map-from-str " { [i, j] -> array[i+j] } "))
+;; #<UNION-MAP { [i, j] -> array[i + j] }>
 
+;; The total of the thing written in the innerloop is [i] -> { array[i0] : i < i0 <= 9 + i }
+(setf all-written-inner (union-set-apply domain-inner vwrite))
+;; #<UNION-SET [i] -> { array[i0] : i < i0 <= 9 + i }>
 
+;; Let's compute the intersection for the innerloop, it's [i] -> { array[1] : -8 <= i <= 0 }
+(setf intersection-inner (union-set-intersect (union-set-apply domain-inner vread) all-written-inner))
+;; #<UNION-SET [i] -> { array[1] : -8 <= i <= 0 }>
 
+;; Because the result depends on i, let's add the information that i is between 1 and 10. The result is the empty set! So we can move this statement outside the innerloop.
+(union-set-intersect-params intersection-inner (set-from-str "[i] -> {: 1 <= i < 10}"))
+;; #<UNION-SET [i] -> {  }>
 
+;; Just to double check, if the outerloop is from 0 to 10, the result would be [i] -> { array[1] : i = 0 }
+(union-set-intersect-params intersection-inner (set-from-str "[i] -> {: 0 <= i < 10}"))
+;; #<UNION-SET [i] -> { array[1] : i = 0 }>
 
+;; Also, the total of the thing written in the outerloop is { array[i0] : 0 < i0 <= 18 and (i0 >= 2 or i0 <= 9) }
+(setf all-written-outer (union-set-union (union-set-apply domain-outer vwrite) (union-set-from-str " {array[i]: 1 <= i < 10}")))
+;; #<UNION-SET { array[i0] : 0 < i0 <= 18 and (i0 >= 2 or i0 <= 9) }>
 
-
-
-
-;; END
-
-(when nil
-(let ((t1 "{TAB[I] -> [I,1] : 0 <= I < 100;T2[I] -> [I,2] : 0 <= I < 100;}")
-      (t2 "{TAB[I,J] -> [I,2,J,3] : 0 <= I < 100 and 0 <= J < 100;}")
-      (t3 "{TAB[I,J] -> [I,3,J,4] : 0 <= I < 100 and 0 <= J < 100;}")
-      (t4 "{C[I,J] -> [I,4,J,5] : 0 <= I < 100 and 0 <= J < 100;C1[I,J] -> [I,4,J,5,K,6] : 0 <= I < 100 and 0 <= J < 100 and 0 <= K < 100;C2[I,J] -> [I,4,J,5,K,7] : 0 <= I < 100 and 0 <= J < 100 and 0 <= K < 100;A[I,K] -> [I,4,J,5,K,8] : 0 <= I < 100 and 0 <= J < 100 and 0 <= K < 100;B[K,J] -> [I,4,J,5,K,9] : 0 <= I < 100 and 0 <= J < 100 and 0 <= K < 100;}")
-      (t5 "[N] -> { S0[i] -> [i, 0] : 0 <= i < N; S1[i] -> [i, 1] : 0 <= i < N }")
-      )
-
-  ;; Previous test
-  (let ((s t4))
-    (print s)
-    (let* ((map (union_map-read-from-str s))
-           (ast-build (ast_build-from-context (set-read-from-str "[N] -> { : }")))
-           (ast-node (ast_build-from-node-from-schedule-map ast-build map)))
-      (print (isl_ast_node_to_C_str (obj ast-node)))))
-
-  ;; Demo with and without the library
-  ;; First, without
-  (let* ((ast-build (isl_ast_build_alloc *context*)) ;;from_context (isl_set_read_from_str *context* "[N] -> { : }")))
-                                        ;(schedule (isl_union_map_read_from_str *context* t1))
-         (schedule (isl_schedule_constraints_on_domain
-                    (isl_union_set_read_from_str *context* "[N] -> { A[i]: 0 <= i < N }")))
-         (_ (break2 "Domain done"))
-         (schedule (isl_schedule_constraints_set_validity
-                    schedule
-                    (isl_union_map_read_from_str *context* "[N] -> { A[i] -> A[i-1]: 0 <= i}")))
-         (_ (break2 "Validity done"))
-         (_ (break2 "End of creation of the schedule"))
-         (schedule (isl_schedule_constraints_compute_schedule schedule))
-         (_ (break2 "Schedule created"))
-         (node (isl_ast_build_node_from_schedule ast-build schedule))
-         (_ (break2 "Ast created"))
-         )
-    (print (isl_ast_node_to_C_str node)))
-
-  ;; And then, with cl-isl
-  (let* ((ast-build (alloc-ast_build))
-         (schedule (create-schedule-on-domain
-                    (union_set-read-from-str "[N] -> { A[i, j]: 0 <= i < N and 0 <= j < N; B[i, j]: 0 <= i < N and 0 <= j < N; RES[i, j]: 0 <= i < N and 0 <= j < N}")))
-         (_ (break2 "Domain done"))
-         (schedule1 (schedule-constraints-set-validity
-                     schedule
-                     (union_map-read-from-str "[N] -> { B[i, j] -> A[i, j] : 0 <= i}")))
-         (_ (break2 "Validity done"))
-         (_ (break2 "End of creation of the schedule"))
-         (schedule2 (schedule-constraints-compute-schedule schedule1))
-         (_ (break2 "Schedule created"))
-         (node (ast_build-node-from-schedule ast-build schedule2))
-         (_ (break2 "Ast created"))
-         )
-    (print (ast_node-to-C-str node)))
-
-  ))
+;; So the intersection with { array[1] } is non empty! So we can't move outside the outerloop
+(union-set-intersect (union-set-from-str " { array[1] }") all-written-outer)
+;; #<UNION-SET { array[1] }>
