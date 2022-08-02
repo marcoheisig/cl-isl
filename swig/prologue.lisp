8bfcda8c23385d632fa96a07d521fa6992e196c1
resttime <resttime@users.noreply.github.com>
2022-08-02 17:35:58 -0500

    Add support for darwin

--- swig/prologue.lisp
+++ swig/prologue.lisp
@@ -1,6 +1,7 @@
 (in-package #:cl-isl)
 
 (cffi:define-foreign-library libisl
+  (:darwin (:or "libisl.dylib" "libisl.22.dylib"))
   (:unix (:or "libisl.so" "libisl.so.22"))
   (t (:default "libisl")))
 
